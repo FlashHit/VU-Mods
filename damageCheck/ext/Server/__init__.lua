59e07e5a3f1858cd83235ab2cb8e3a06cb25718a
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-01-17 18:09:11 +0100

    Update __init__.lua

--- damageCheck/ext/Server/__init__.lua
+++ damageCheck/ext/Server/__init__.lua
@@ -1,5 +1,4 @@
 local materialGrid = nil
-local boneToMaterialMap = nil
 		
 Hooks:Install('Soldier:Damage', 1, function(hook, soldier, info, giverInfo)
 	if giverInfo == nil or giverInfo.weaponFiring == nil or giverInfo.giver == nil or giverInfo.giver.soldier == nil then
@@ -11,56 +10,67 @@ Hooks:Install('Soldier:Damage', 1, function(hook, soldier, info, giverInfo)
 	if info.isBulletDamage == false then
 		return
 	end
-	if (info.boneIndex >= 0 and info.boneIndex <= 5) then
 
-		local bullet = BulletEntityData(WeaponFiringData(giverInfo.weaponFiring).primaryFire.shot.projectileData)
-		if giverInfo.giver.soldier.weaponsComponent.currentWeapon.weaponModifier.weaponProjectileModifier ~= nil and giverInfo.giver.soldier.weaponsComponent.currentWeapon.weaponModifier.weaponProjectileModifier.projectileData ~= nil then
-			bullet = BulletEntityData(giverInfo.giver.soldier.weaponsComponent.currentWeapon.weaponModifier.weaponProjectileModifier.projectileData)
-		end
-		local materialIndexMapIndex = bullet.materialPair.physicsPropertyIndex
-		if materialIndexMapIndex < 0 then
-			materialIndexMapIndex = 256 + materialIndexMapIndex
-		end
-		local materialGridItems = MaterialInteractionGridRow(materialGrid.interactionGrid[materialGrid.materialIndexMap[materialIndexMapIndex+1]+1]).items
-		local multiplier = MaterialRelationDamageData(materialGridItems[boneToMaterialMap[info.boneIndex+1]+1].physicsPropertyProperties[1]).damageProtectionMultiplier
-		local damageLimit = bullet.startDamage * multiplier 
-		
-		
-		local shotDistance = info.position:Distance(info.origin)
-		
-		if (shotDistance >= bullet.damageFalloffEndDistance) then
+	local bullet = BulletEntityData(WeaponFiringData(giverInfo.weaponFiring).primaryFire.shot.projectileData)
+	if giverInfo.giver.soldier.weaponsComponent.currentWeapon.weaponModifier.weaponProjectileModifier ~= nil and giverInfo.giver.soldier.weaponsComponent.currentWeapon.weaponModifier.weaponProjectileModifier.projectileData ~= nil then
+		bullet = BulletEntityData(giverInfo.giver.soldier.weaponsComponent.currentWeapon.weaponModifier.weaponProjectileModifier.projectileData)
+	end
 
-			damageLimit = bullet.endDamage * multiplier
+	local materialIndexMapIndex1 = bullet.materialPair.physicsPropertyIndex
+	if materialIndexMapIndex1 < 0 then
+		materialIndexMapIndex1 = 256 + materialIndexMapIndex1
+	end
 
-		elseif (shotDistance > bullet.damageFalloffStartDistance) then
-			
-			local damageFalloffMax = (bullet.startDamage - bullet.endDamage)
-			-- 25.0 - 18.4 = 6.6
-			
-			local distanceRange = (bullet.damageFalloffEndDistance - bullet.damageFalloffStartDistance)
-			-- 50.0 - 8.0 = 42.0
-			
-			local distancePercentage = (shotDistance - bullet.damageFalloffStartDistance) / distanceRange
-			-- 50.0 - 8.0 / 42.0 = 1.00
-			
-			damageLimit = bullet.startDamage - (damageFalloffMax * distancePercentage) * multiplier
-			-- 25.0 - (6.6 * 1.00) * 1.0 = 18.4
-		
+	local materialIndexMapIndex2 = MaterialContainerPair(info.damagedMaterial).physicsPropertyIndex
+	if materialIndexMapIndex2 < 0 then
+		materialIndexMapIndex2 = 256 + materialIndexMapIndex2
+	end
+
+	local materialGridItems = MaterialInteractionGridRow(materialGrid.interactionGrid[materialGrid.materialIndexMap[materialIndexMapIndex1+1]+1]).items
+	local materialRelationDamageData = materialGridItems[materialGrid.materialIndexMap[materialIndexMapIndex2+1]+1]
+
+	local multiplier = 1.0
+	for i,_ in pairs(materialRelationDamageData.physicsPropertyProperties) do
+		if materialRelationDamageData:Is('MaterialRelationDamageData') then
+			multiplier = MaterialRelationDamageData(materialRelationDamageData).physicsPropertyProperties[i].damageProtectionMultiplier
 		end
-		
-		if (damageLimit < info.damage - 0.5) then
-			info.damage = damageLimit
-			hook:Pass(soldier, info, giverInfo)
-		end
+	end
+
+	local damageLimit = bullet.startDamage * multiplier 
+
+
+	local shotDistance = info.position:Distance(info.origin)
+
+	if (shotDistance >= bullet.damageFalloffEndDistance) then
+
+		damageLimit = bullet.endDamage * multiplier
+
+	elseif (shotDistance > bullet.damageFalloffStartDistance) then
+
+		local damageFalloffMax = (bullet.startDamage - bullet.endDamage)
+		-- 25.0 - 18.4 = 6.6
+
+		local distanceRange = (bullet.damageFalloffEndDistance - bullet.damageFalloffStartDistance)
+		-- 50.0 - 8.0 = 42.0
+
+		local distancePercentage = (shotDistance - bullet.damageFalloffStartDistance) / distanceRange
+		-- 50.0 - 8.0 / 42.0 = 1.00
+
+		damageLimit = bullet.startDamage - (damageFalloffMax * distancePercentage) * multiplier
+		-- 25.0 - (6.6 * 1.00) * 1.0 = 18.4
+
+	end
+
+	if (damageLimit < info.damage - 0.5) then
+		info.damage = damageLimit
+		hook:Pass(soldier, info, giverInfo)
 	end
 end)
 
 Events:Subscribe('Level:Loaded', function(levelName, gameMode, round, roundsPerMap)
 	materialGrid = MaterialGridData(ResourceManager:SearchForDataContainer(SharedUtils:GetLevelName() .. "/MaterialGrid_Win32/Grid"))
-	boneToMaterialMap = { materialGrid.materialIndexMap[158+1], materialGrid.materialIndexMap[65+1], materialGrid.materialIndexMap[66+1], materialGrid.materialIndexMap[66+1], materialGrid.materialIndexMap[91+1], materialGrid.materialIndexMap[91+1] }
 end)
 
 Events:Subscribe('Level:Destroy', function()
-	boneToMaterialMap = nil
 	materialGrid = nil
 end) 
