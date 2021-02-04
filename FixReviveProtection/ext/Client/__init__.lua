4c9d3d1f9a955ff696ccb3f83733e6d0b4f60616
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-02-04 19:20:16 +0100

    update revive protection mod

--- FixReviveProtection/ext/Client/__init__.lua
+++ FixReviveProtection/ext/Client/__init__.lua
@@ -1,5 +1,9 @@
 Hooks:Install('BulletEntity:Collision', 1, function(hook, entity, hit, shooter)
-    if hit.rigidBody:Is('RagdollEntity') then
+    if hit.rigidBody:Is('RagdollEntity') 
+	and PhysicsEntityBase(hit.rigidBody).sleeping == true
+	and PhysicsEntityBase(hit.rigidBody).userData ~= nil 
+	and PhysicsEntityBase(hit.rigidBody).userData:Is('ClientSoldierEntity') 
+	and SoldierEntity(PhysicsEntityBase(hit.rigidBody).userData).internalHealth > 20 then
 		hook:Return()
 	end
 end)
