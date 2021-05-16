981f3dab2b7180ac49a88df4eae1f5456bf85849
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-05-16 17:53:19 +0200

    add missing input restrictions to No-PreRound mod

--- No-PreRound/ext/Server/__init__.lua
+++ No-PreRound/ext/Server/__init__.lua
@@ -49,10 +49,14 @@ Events:Subscribe('Level:Loaded', function()
 	local inputRestrictionEntity = inputRestrictionIterator:Next()
 	while inputRestrictionEntity do
 		if inputRestrictionEntity.data.instanceGuid == Guid('E8C37E6A-0C8B-4F97-ABDD-28715376BD2D') or -- cq / cq assault / tank- / air superiority
+		inputRestrictionEntity.data.instanceGuid == Guid('593710B7-EDC4-4EDB-BE20-323E7B0CE023') or -- tdm XP4
 		inputRestrictionEntity.data.instanceGuid == Guid('6F42FBE3-428A-463A-9014-AA0C6E09DA64') or -- tdm
 		inputRestrictionEntity.data.instanceGuid == Guid('9EDC59FB-5821-4A37-A739-FE867F251000') or -- rush / sq rush
 		inputRestrictionEntity.data.instanceGuid == Guid('BF4003AC-4B85-46DC-8975-E6682815204D') or -- domination / scavenger
+		inputRestrictionEntity.data.instanceGuid == Guid('A0158B87-FA34-4ED2-B752-EBFC1A34B081') or -- gunmaster XP4
 		inputRestrictionEntity.data.instanceGuid == Guid('AAF90FE3-D1CA-4CFE-84F3-66C6146AD96F') or -- gunmaster
+		inputRestrictionEntity.data.instanceGuid == Guid('753BD81F-07AC-4140-B05C-24210E1DF3FA') or -- sqdm XP4
+		inputRestrictionEntity.data.instanceGuid == Guid('CBFB0D7E-8561-4216-9AB2-99E14E9D18D0') or -- sqdm noVehicles
 		inputRestrictionEntity.data.instanceGuid == Guid('A40B08B7-D781-487A-8D0C-2E1B911C1949') then -- sqdm
 			inputRestrictionEntity = Entity(inputRestrictionEntity)
 			inputRestrictionEntity:FireEvent('Disable')
