bd46e1c6690b858bdf6110748cb947675a792d0d
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-05-27 00:15:07 +0200

    Fix typo

--- NoSpawnProtection/ext/Shared/__init__.lua
+++ NoSpawnProtection/ext/Shared/__init__.lua
@@ -1,5 +1,5 @@
 ResourceManager:RegisterInstanceLoadHandler(Guid('F256E142-C9D8-4BFE-985B-3960B9E9D189'), Guid('705967EE-66D3-4440-88B9-FEEF77F53E77'), function(p_Instance)
-	p_Instance = VeniceSoldierHealthModuleData(instance)
+	p_Instance = VeniceSoldierHealthModuleData(p_Instance)
 	p_Instance:MakeWritable()
 	p_Instance.immortalTimeAfterSpawn = 0
 end)
