f4c6a6159fefb09463c63fcf2ff0f18f625a8776
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-05-27 15:31:50 +0200

    fix revive was not possible

--- FixReviveProtection/ext/Client/__init__.lua
+++ FixReviveProtection/ext/Client/__init__.lua
@@ -1,9 +1,12 @@
-Hooks:Install('BulletEntity:Collision', 1, function(hook, entity, hit, shooter)
-    if hit.rigidBody:Is('RagdollEntity') 
-	and PhysicsEntityBase(hit.rigidBody).sleeping == true
-	and PhysicsEntityBase(hit.rigidBody).userData ~= nil 
-	and PhysicsEntityBase(hit.rigidBody).userData:Is('ClientSoldierEntity') 
-	and SoldierEntity(PhysicsEntityBase(hit.rigidBody).userData).internalHealth > 20 then
-		hook:Return()
+Hooks:Install('BulletEntity:Collision', 1, function(p_HookCtx, p_Entity, p_Hit, p_Shooter)
+    if p_Entity.data ~= nil and p_Entity.data.instanceGuid == Guid("1861554A-8C81-4944-96D1-7347494F7688") then
+		return
+	end
+	if p_Hit.rigidBody:Is('RagdollEntity')
+	and PhysicsEntityBase(p_Hit.rigidBody).sleeping == true
+	and PhysicsEntityBase(p_Hit.rigidBody).userData ~= nil
+	and PhysicsEntityBase(p_Hit.rigidBody).userData:Is('ClientSoldierEntity')
+	and SoldierEntity(PhysicsEntityBase(p_Hit.rigidBody).userData).internalHealth > 20 then
+		p_HookCtx:Return()
 	end
 end)
