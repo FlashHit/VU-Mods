759962d48231c55678bba3ef3d39b2a43b69bfc4
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-05-27 15:32:10 +0200

    Update __init__.lua

--- FixReviveProtection/ext/Client/__init__.lua
+++ FixReviveProtection/ext/Client/__init__.lua
@@ -1,5 +1,5 @@
 Hooks:Install('BulletEntity:Collision', 1, function(p_HookCtx, p_Entity, p_Hit, p_Shooter)
-    if p_Entity.data ~= nil and p_Entity.data.instanceGuid == Guid("1861554A-8C81-4944-96D1-7347494F7688") then
+	if p_Entity.data ~= nil and p_Entity.data.instanceGuid == Guid("1861554A-8C81-4944-96D1-7347494F7688") then
 		return
 	end
 	if p_Hit.rigidBody:Is('RagdollEntity')
