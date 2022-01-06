e70f8cc7ca54f4db8027c94311715fceb2b6c736
FlashHit <56718716+FlashHit@users.noreply.github.com>
2022-01-06 03:45:55 +0100

    update FixEuroHipfire

--- FixEuroHipfire/ext/Client/__init__.lua
+++ FixEuroHipfire/ext/Client/__init__.lua
@@ -1,7 +1,13 @@
-Events:Subscribe('GunSway:Update', function(gunSway, weapon, weaponFiring, deltaTime)
-	if InputManager:IsDown(InputConceptIdentifiers.ConceptZoom) == false then
-		if gunSway.dispersionAngle < gunSway.minDispersionAngle then
-			gunSway.dispersionAngle = gunSway.minDispersionAngle
-		end
+---@param p_GunSway GunSway
+---@param p_Weapon Entity|nil
+---@param p_WeaponFiring WeaponFiring|nil
+---@param p_DeltaTime number
+Events:Subscribe('GunSway:UpdateRecoil', function(p_GunSway, p_Weapon, p_WeaponFiring, p_DeltaTime)
+	if p_GunSway.dispersionAngle < p_GunSway.minDispersionAngle then
+		p_GunSway.dispersionAngle = p_GunSway.minDispersionAngle
+	end
+
+	if p_GunSway.isFiring and p_WeaponFiring ~= nil and p_WeaponFiring.weaponState <= WeaponState.NoTrigger then
+		p_GunSway.isFiring = false
 	end
 end)
