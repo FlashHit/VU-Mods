ccba9e3e407331704b743d00a58172df9d485e5d
FlashHit <56718716+FlashHit@users.noreply.github.com>
2022-02-26 21:09:49 +0100

    update FastLoad

--- FastLoad/ext/Shared/__init__.lua
+++ FastLoad/ext/Shared/__init__.lua
@@ -1,31 +1,36 @@
--- Your shared code here
-local m_InitialSimRate = 30.0
+local m_InitialSimRate = nil
 
-Events:Subscribe('Level:LoadResources', function()
+local function _SetSimFPS()
 	---@type DataContainer|GameTimeSettings|nil
 	local s_GameTimeSettings = ResourceManager:GetSettings('GameTimeSettings')
 
 	if s_GameTimeSettings ~= nil then
 		s_GameTimeSettings = GameTimeSettings(s_GameTimeSettings)
+
+		if s_GameTimeSettings.maxSimFps > 200.0 then
+			return
+		end
+
 		m_InitialSimRate = s_GameTimeSettings.maxSimFps
 		s_GameTimeSettings.maxSimFps = 10000.0
 	end
-end)
+end
 
-local function ResetSimFPS()
+local function _ResetSimFPS()
+	if m_InitialSimRate == nil then
+		return
+	end
+
 	---@type DataContainer|GameTimeSettings|nil
 	local s_GameTimeSettings = ResourceManager:GetSettings('GameTimeSettings')
 
 	if s_GameTimeSettings ~= nil then
 		s_GameTimeSettings = GameTimeSettings(s_GameTimeSettings)
 		s_GameTimeSettings.maxSimFps = m_InitialSimRate
+		m_InitialSimRate = nil
 	end
 end
 
-Events:Subscribe('Level:Loaded', function()
-	ResetSimFPS()
-end)
-
-Events:Subscribe('Extension:Unloading', function()
-	ResetSimFPS()
-end)
+Events:Subscribe('Level:LoadResources', _SetSimFPS)
+Events:Subscribe('Level:Loaded', _ResetSimFPS)
+Events:Subscribe('Extension:Unloading', _ResetSimFPS)
