45d62ac89157485cda1f15c19812710889ed5a3a
FlashHit <56718716+FlashHit@users.noreply.github.com>
2022-02-28 23:15:49 +0100

    add PropertyChanged example

---
+++ PropertyChanged/ext/Server/__init__.lua
@@ -0,0 +1,19 @@
+---@param p_Player Player|nil
+---@param p_RecipientMask integer
+---@param p_Message string
+Events:Subscribe('Player:Chat', function(p_Player, p_RecipientMask, p_Message)
+	if tonumber(p_Message) then
+		local s_EntityIterator = EntityManager:GetIterator('PropertyCastEntity')
+		local s_Entity = s_EntityIterator:Next()
+
+		while s_Entity do
+			if s_Entity.data and s_Entity.data.instanceGuid == Guid("51A231A1-CCBA-3DEF-1E3B-A28F5AE67188") then
+				print("set property")
+				s_Entity:PropertyChanged("FloatValue", tonumber(p_Message))
+				return
+			end
+
+			s_Entity = s_EntityIterator:Next()
+		end
+	end
+end)
