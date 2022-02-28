22effb43346a197ef20c05ef9b243a919340a972
FlashHit <56718716+FlashHit@users.noreply.github.com>
2022-02-28 23:21:14 +0100

    fix: only modify it for the one who wrote into the chat

--- PropertyChanged/ext/Server/__init__.lua
+++ PropertyChanged/ext/Server/__init__.lua
@@ -2,12 +2,12 @@
 ---@param p_RecipientMask integer
 ---@param p_Message string
 Events:Subscribe('Player:Chat', function(p_Player, p_RecipientMask, p_Message)
-	if tonumber(p_Message) then
+	if tonumber(p_Message) and p_Player.soldier then
 		local s_EntityIterator = EntityManager:GetIterator('PropertyCastEntity')
 		local s_Entity = s_EntityIterator:Next()
 
 		while s_Entity do
-			if s_Entity.data and s_Entity.data.instanceGuid == Guid("51A231A1-CCBA-3DEF-1E3B-A28F5AE67188") then
+			if s_Entity.data and s_Entity.data.instanceGuid == Guid("51A231A1-CCBA-3DEF-1E3B-A28F5AE67188") and s_Entity.bus == p_Player.soldier.bus then
 				print("set property")
 				s_Entity:PropertyChanged("FloatValue", tonumber(p_Message))
 				return
